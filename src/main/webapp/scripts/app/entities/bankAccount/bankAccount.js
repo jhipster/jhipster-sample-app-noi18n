bf1e186622a97a06f69bec417e5505707c1549f4
Julien Dubois <julien.dubois@gmail.com>
2015-08-06 16:43:32 +0200

    automatic project update

---
+++ src/main/webapp/scripts/app/entities/bankAccount/bankAccount.js
@@ -0,0 +1,87 @@
+'use strict';
+
+angular.module('jhipsterApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('bankAccount', {
+                parent: 'entity',
+                url: '/bankAccounts',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'BankAccounts'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/bankAccount/bankAccounts.html',
+                        controller: 'BankAccountController'
+                    }
+                },
+                resolve: {
+                }
+            })
+            .state('bankAccount.detail', {
+                parent: 'entity',
+                url: '/bankAccount/{id}',
+                data: {
+                    roles: ['ROLE_USER'],
+                    pageTitle: 'BankAccount'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/bankAccount/bankAccount-detail.html',
+                        controller: 'BankAccountDetailController'
+                    }
+                },
+                resolve: {
+                    entity: ['$stateParams', 'BankAccount', function($stateParams, BankAccount) {
+                        return BankAccount.get({id : $stateParams.id});
+                    }]
+                }
+            })
+            .state('bankAccount.new', {
+                parent: 'bankAccount',
+                url: '/new',
+                data: {
+                    roles: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$modal', function($stateParams, $state, $modal) {
+                    $modal.open({
+                        templateUrl: 'scripts/app/entities/bankAccount/bankAccount-dialog.html',
+                        controller: 'BankAccountDialogController',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {name: null, balance: null, id: null};
+                            }
+                        }
+                    }).result.then(function(result) {
+                        $state.go('bankAccount', null, { reload: true });
+                    }, function() {
+                        $state.go('bankAccount');
+                    })
+                }]
+            })
+            .state('bankAccount.edit', {
+                parent: 'bankAccount',
+                url: '/{id}/edit',
+                data: {
+                    roles: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$modal', function($stateParams, $state, $modal) {
+                    $modal.open({
+                        templateUrl: 'scripts/app/entities/bankAccount/bankAccount-dialog.html',
+                        controller: 'BankAccountDialogController',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['BankAccount', function(BankAccount) {
+                                return BankAccount.get({id : $stateParams.id});
+                            }]
+                        }
+                    }).result.then(function(result) {
+                        $state.go('bankAccount', null, { reload: true });
+                    }, function() {
+                        $state.go('^');
+                    })
+                }]
+            });
+    });
