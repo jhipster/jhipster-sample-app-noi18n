a08ed3dc71ece8c535056c7a5c1c5a90b6cdbc86
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:32:56 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pager.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleNo18nApp')
-    .directive('sampleNo18nAppPager', function() {
-        return {
-            templateUrl: 'scripts/components/form/pager.html'
-        };
-    });
--- src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleNo18nApp')
-    .directive('sampleNo18nAppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });
