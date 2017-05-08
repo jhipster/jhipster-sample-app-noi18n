a5f48a7d38810fa9f4c0ff0fdff971993243f1f8
Julien Dubois <julien.dubois@gmail.com>
2017-05-08 14:49:08 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/web/rest/util/HeaderUtil.java
+++ src/main/java/io/github/jhipster/sample/web/rest/util/HeaderUtil.java
@@ -33,7 +33,7 @@ public final class HeaderUtil {
     }
 
     public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
-        log.error("Entity creation failed, {}", defaultMessage);
+        log.error("Entity processing failed, {}", defaultMessage);
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-jhipsterNoI18NSampleApplicationApp-error", defaultMessage);
         headers.add("X-jhipsterNoI18NSampleApplicationApp-params", entityName);
