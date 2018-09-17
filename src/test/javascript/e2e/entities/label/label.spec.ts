04a30c385e5a4a056273ab05c88fad755b597dc9
Julien Dubois <julien.dubois@gmail.com>
2018-09-17 23:25:51 +0200

    automatic project update

--- src/test/javascript/e2e/entities/label/label.spec.ts
+++ src/test/javascript/e2e/entities/label/label.spec.ts
@@ -1,7 +1,10 @@
+/* tslint:disable no-unused-expression */
 import { browser, ExpectedConditions as ec } from 'protractor';
 import { NavBarPage, SignInPage } from '../../page-objects/jhi-page-objects';
 
 import { LabelComponentsPage, LabelDeleteDialog, LabelUpdatePage } from './label.page-object';
+
+const expect = chai.expect;
 
 describe('Label e2e test', () => {
     let navBarPage: NavBarPage;
@@ -10,7 +13,7 @@ describe('Label e2e test', () => {
     let labelComponentsPage: LabelComponentsPage;
     let labelDeleteDialog: LabelDeleteDialog;
 
-    beforeAll(async () => {
+    before(async () => {
         await browser.get('/');
         navBarPage = new NavBarPage();
         signInPage = await navBarPage.getSignInPage();
@@ -21,22 +24,26 @@ describe('Label e2e test', () => {
     it('should load Labels', async () => {
         await navBarPage.goToEntity('label');
         labelComponentsPage = new LabelComponentsPage();
-        expect(await labelComponentsPage.getTitle()).toMatch(/Labels/);
+        expect(await labelComponentsPage.getTitle()).to.eq('Labels');
     });
 
     it('should load create Label page', async () => {
         await labelComponentsPage.clickOnCreateButton();
         labelUpdatePage = new LabelUpdatePage();
-        expect(await labelUpdatePage.getPageTitle()).toMatch(/Create or edit a Label/);
+        expect(await labelUpdatePage.getPageTitle()).to.eq('Create or edit a Label');
         await labelUpdatePage.cancel();
     });
 
     it('should create and save Labels', async () => {
+        const nbButtonsBeforeCreate = await labelComponentsPage.countDeleteButtons();
+
         await labelComponentsPage.clickOnCreateButton();
         await labelUpdatePage.setLabelInput('label');
-        expect(await labelUpdatePage.getLabelInput()).toMatch('label');
+        expect(await labelUpdatePage.getLabelInput()).to.eq('label');
         await labelUpdatePage.save();
-        expect(await labelUpdatePage.getSaveButton().isPresent()).toBeFalsy();
+        expect(await labelUpdatePage.getSaveButton().isPresent()).to.be.false;
+
+        expect(await labelComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeCreate + 1);
     });
 
     it('should delete last Label', async () => {
@@ -44,13 +51,13 @@ describe('Label e2e test', () => {
         await labelComponentsPage.clickOnLastDeleteButton();
 
         labelDeleteDialog = new LabelDeleteDialog();
-        expect(await labelDeleteDialog.getDialogTitle()).toMatch(/Are you sure you want to delete this Label?/);
+        expect(await labelDeleteDialog.getDialogTitle()).to.eq('Are you sure you want to delete this Label?');
         await labelDeleteDialog.clickOnConfirmButton();
 
-        expect(await labelComponentsPage.countDeleteButtons()).toBe(nbButtonsBeforeDelete - 1);
+        expect(await labelComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeDelete - 1);
     });
 
-    afterAll(async () => {
+    after(async () => {
         await navBarPage.autoSignOut();
     });
 });
