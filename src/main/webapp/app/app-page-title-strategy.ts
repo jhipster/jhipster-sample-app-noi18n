6e4f3d5b25303aa94a0a8b5894f6e977620c7d93
Matt Raible <matt@raibledesigns.com>
2025-03-31 21:28:22 -0600

    automatic project update for v8.10.0

--- src/main/webapp/app/app-page-title-strategy.ts
+++ src/main/webapp/app/app-page-title-strategy.ts
@@ -5,9 +5,7 @@ import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 export class AppPageTitleStrategy extends TitleStrategy {
   override updateTitle(routerState: RouterStateSnapshot): void {
     let pageTitle = this.buildTitle(routerState);
-    if (!pageTitle) {
-      pageTitle = 'Jhipster No I 18 N Sample Application';
-    }
+    pageTitle ??= 'Jhipster No I 18 N Sample Application';
     document.title = pageTitle;
   }
 }
